f6b2fc815dea2bd8cd4d542985a04e213c4bd449
johnkyky <ugo.battiston@protonmail.com>
2023-12-13 14:56:07 +0100

    bug fix

--- perfCounter.cpp
+++ perfCounter.cpp
@@ -7,3 +7,14 @@ int perf_event_open(struct perf_event_attr *hw, pid_t pid, int cpu, int grp,
                     unsigned long flags) {
   return syscall(__NR_perf_event_open, hw, pid, cpu, grp, flags);
 }
+
+void PerfCounter::open(const pid_t pid, const int cpu, const int grp,
+                       const unsigned long flags) {
+  asm volatile("nop;"); // pseudo-barrier
+  fd = syscall(__NR_perf_event_open, &attr, pid, cpu, grp, flags);
+  asm volatile("nop;"); // pseudo-barrier
+  if (fd < 0)
+    throw std::runtime_error("Error: Open PerfData::open");
+}
+
+void PerfCounter::open() { this->open(getpid(), -1, -1, 0); }
--- perfCounter.h
+++ perfCounter.h
@@ -86,15 +86,9 @@ public:
   }
 
   void open(const pid_t pid, const int cpu, const int grp,
-            const unsigned long flags) {
-    asm volatile("nop;"); // pseudo-barrier
-    fd = syscall(__NR_perf_event_open, &attr, pid, cpu, grp, flags);
-    asm volatile("nop;"); // pseudo-barrier
-    if (fd < 0)
-      throw std::runtime_error("Error: Open PerfData::open");
-  }
+            const unsigned long flags);
 
-  void open() { this->open(getpid(), -1, -1, 0); }
+  void open();
 
   void start() {
     counting = true;
